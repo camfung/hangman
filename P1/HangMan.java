2756fd529d27c2eac02a5f8c38cb590d8da91ce6
Cameron Fung <Cameronfuung@gmail.com>
2022-09-21 17:22:02 -0700

    first commit

---
+++ P1/HangMan.java
@@ -0,0 +1,77 @@
+package P1;
+import java.util.*;
+
+public class HangMan {
+	final static String[] wordBank = {"thunder", "lightning", "cloud", "capital", "washington", "weather"};
+	
+	
+	
+	public static void main(String[] args) {
+		List<Character> guesses = new ArrayList<>();
+		List<Character> anslst = new ArrayList<>();
+		List<Character> guessWord = new ArrayList<>();
+		
+		String[] man = {" O\n", "/", "|", "\\\n", "/", " \\\n"};  
+		Scanner input = new Scanner(System.in);
+		
+		int wrongGuesses = 0;
+		int correctGuesses = 0;
+		
+		// getting random word from bank
+		Random gen = new Random();
+		int index; 
+		index = gen.nextInt(wordBank.length);
+		String answer = wordBank[index];
+		
+		for (int i = 0; i < answer.length(); i++){
+			anslst.add(answer.charAt(i));
+			guessWord.add('_');
+		}
+		
+		while (true) {
+			boolean goodGuess = false;
+			
+			System.out.println("Enter your guess:");
+			
+			for (int i = 0; i < answer.length(); i++){
+				System.out.print(guessWord.get(i) + " ");
+			}
+			
+			System.out.println();
+			char guess = input.next().charAt(0);
+			
+			for (int i = 0 ; i < answer.length(); i++){
+				if (guess == anslst.get(i)) {
+					guessWord.set(i, guess);
+					goodGuess = true;
+				}
+			}
+			
+			if (!goodGuess) {
+				System.out.println("wrong guess");
+				wrongGuesses++;
+				System.out.println("Wrong Guesses:" + wrongGuesses);
+				
+				 for (int i = 0; i < wrongGuesses; i++)
+				 {
+					 System.out.print(man[i]);
+				 }
+	
+				 System.out.println();
+			}
+			else {
+				correctGuesses++;
+			}
+			if (wrongGuesses > 5)
+			{
+				System.out.println("you lose! \nThe word was " 
+									+ answer + ".");	
+				break;
+			}
+			if (correctGuesses >= answer.length()) {
+				System.out.println("you won!" + answer.length());
+			}
+			
+		}
+	}
+}
